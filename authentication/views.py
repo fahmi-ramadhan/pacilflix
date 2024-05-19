6b356117e1388bd1fa45b357a1da84b041513012
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-05-19 21:20:05 +0700

    fix: redirect issue in authentication index view

--- authentication/views.py
+++ authentication/views.py
@@ -9,7 +9,7 @@ from django.views.decorators.http import require_http_methods
 @require_http_methods(['GET'])
 def index(request):
     if request.session.get('username') is not None:
-        return redirect('show')
+        return redirect('show:tayangan')
     return render(request, 'authentication/index.html')
 
 
